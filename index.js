a499bab33746c9d7bb903f38324555d714f75942
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-10-25 21:03:55 -1000

    Update index.js

--- index.js
+++ index.js
@@ -106,6 +106,7 @@ express()
           }
           const keys = Object.keys(fields);
 		  console.log("keys = " + keys);
+		  console.log("typeof keys = " + typeof keys);
 		  const splitarray = keys.split("_");
 		  res.render("game_board", { rows:rows, cols:cols, open_rr:splitarray[1], open_cc:splitarray[2], val:"E" });
       })//form.parse
